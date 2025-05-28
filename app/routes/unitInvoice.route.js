fb60a0d2f75944e8b11b0e72985e5e4b6f36f403
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-05-28 09:24:26 +0700

    Add ':id' in unitInvoice route

--- app/routes/unitInvoice.route.js
+++ app/routes/unitInvoice.route.js
@@ -5,8 +5,8 @@ const { authenticateToken, checkRoleMiddleware } = require('../middlewares/auth.
 
 router.get('/', unitInvoiceController.getAllUnitInvoice)
 router.post('/', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), unitInvoiceController.createUnitInvoice)
-router.put('/', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), unitInvoiceController.updateUnitInvoice)
-router.delete('/', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), unitInvoiceController.deleteUnitInvoice)
+router.put('/:id', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), unitInvoiceController.updateUnitInvoice)
+router.delete('/:id', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), unitInvoiceController.deleteUnitInvoice)
 router.get('/:id', unitInvoiceController.getUnitInvoiceById)
 
 module.exports= router
