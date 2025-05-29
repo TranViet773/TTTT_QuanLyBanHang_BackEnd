79310964049a358ec2fa5ccfa463b5b0c9417f5a
DoThanhDuB2111834 <dub2111834@student.ctu.edu.vn>
2025-05-29 14:14:06 +0700

    fix bug get item by name

--- app/routes/itemType.route.js
+++ app/routes/itemType.route.js
@@ -4,9 +4,10 @@ const itemTypeController = require('../controllers/itemType.controller');
 const { authenticateToken, checkRoleMiddleware } = require('../middlewares/auth.middleware');
 
 router.get('/', itemTypeController.getAllItemTypes);
+router.get('/name/', itemTypeController.getItemTypeByName);
 router.get('/:id', itemTypeController.getItemTypeById);
 router.post('/', authenticateToken, itemTypeController.createItemType);
 router.put('/:id', authenticateToken, itemTypeController.updateItemType);
 router.delete('/:id', authenticateToken, itemTypeController.deleteItemType);
-router.get('/name', itemTypeController.getItemTypeByName);
+
 module.exports = router;
