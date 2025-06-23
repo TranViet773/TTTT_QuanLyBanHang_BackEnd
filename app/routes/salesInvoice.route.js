f7b1e5aa30fdb98b974655c4c3f773807286b08b
B2110046 <honglam1115@gmail.com>
2025-06-23 09:11:07 +0700

    modify sales invoice routes

--- app/routes/salesInvoice.route.js
+++ app/routes/salesInvoice.route.js
@@ -14,8 +14,6 @@ router.get('/statistic-based-on-status', authenticateToken, checkRoleMiddleware(
 router.get('/statistic-revenue-last-seven-days', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), salesInvoiceController.statisticsRevenueLast7Days);
 router.get('/statistic-revenue-last-four-weeks', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), salesInvoiceController.statisticsRevenueLast4Weeks);
 router.get('/statistic-revenue-last-four-months', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), salesInvoiceController.statisticsRevenueLast4Months);
-
-router.get('/:invoiceCode', authenticateToken, salesInvoiceController.getInvoiceByCode)
 router.get('/:invoiceCode', authenticateToken, salesInvoiceController.getInvoiceByCode)
 router.delete('/:invoiceCode', authenticateToken, checkRoleMiddleware(['admin', 'manager', 'staff']), salesInvoiceController.deleteInvoice)
 
